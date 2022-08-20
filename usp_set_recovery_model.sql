496266a37f1ffce3258d54447461c41333af19f9
Aaron Priesterroth <aaron@madafa.de>
2022-08-20 17:28:51 +0200

    Add usp_set_recovery_model.sql

---
+++ usp_set_recovery_model.sql
@@ -0,0 +1,74 @@
+﻿
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+/*
+
+ ╭────────────────────────────────────────────────────────────────────────────────────────────────╮
+ │                                                                                                │
+ │          AUTHOR:  Aaron Priesterroth                                                           │
+ │         ────────                                                                               │
+ │                                                                                                │
+ │     CREATE DATE:  20/08/2022                                                                   │
+ │    ─────────────                                                                               │
+ │                                                                                                │
+ │     DESCRIPTION:  This procedure sets the recovery model of every user database to a specific  │
+ │    ─────────────  option. The options are FULL, BULK_LOGGED, or SIMPLE.                        │
+ │                                                                                                │
+ │                   If no recovery model option is specified, the option FULL will be used.      │
+ │                                                                                                │
+ ├────────────────────────────────────────────────────────────────────────────────────────────────┤
+ │                                                                                                │
+ │                                                                                                │
+ │  CHANGE HISTORY:                                                                               │
+ │  ───────────────                                                                               │
+ │                                                                                                │
+ │                                                                                                │
+ │    DATE          AUTHOR                            COMMENT                                     │
+ │   ────────────  ───────────────────────────────── ──────────────────────────────────────────   │
+ │    2022-08-20    Aaron Priesterroth                Initial creation.                           │
+ │                                                                                                │
+ │                                                                                                │
+ ╰────────────────────────────────────────────────────────────────────────────────────────────────╯
+
+*/
+
+CREATE OR ALTER   PROCEDURE [dbo].[usp_set_recovery_model]
+       @recovery_model NVARCHAR(100) = 'FULL'
+AS
+BEGIN
+       SET NOCOUNT ON;
+
+       DECLARE @sql     NVARCHAR(MAX);
+       DECLARE @db_name NVARCHAR(MAX);
+
+       IF (@recovery_model IN ('FULL', 'BULK_LOGGED', 'SIMPLE'))
+       BEGIN
+             SELECT [name] INTO [#dbs] FROM [sys].[databases] WHERE [database_id] > 4 AND [recovery_model_desc] <> @recovery_model;
+
+             WHILE(EXISTS(SELECT 1 FROM #dbs))
+             BEGIN
+                    SET @db_name = (SELECT TOP(1) [name] FROM [#dbs] ORDER BY [name]);
+
+                    SET @sql = 'USE [master]; ALTER DATABASE ' + QUOTENAME(@db_name) + ' SET RECOVERY ' + @recovery_model + ' WITH NO_WAIT;';
+                    EXEC sp_executesql @sql;
+
+                    DELETE FROM [#dbs] WHERE [name] = @db_name;
+             END;
+
+             DROP TABLE [#dbs];
+
+       END;
+       ELSE
+       BEGIN
+             PRINT '
+ERROR:
+Recovery model ' + QUOTENAME(@recovery_model) + ' is unknown.';
+       END;
+END
+GO
+
+
