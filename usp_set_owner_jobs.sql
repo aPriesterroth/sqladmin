03c669a8c6305c76387a40c5e888be912ea858c1
Aaron Priesterroth <aaron@madafa.de>
2022-08-20 17:28:37 +0200

    Add usp_set_owner_job.sql

---
+++ usp_set_owner_jobs.sql
@@ -0,0 +1,71 @@
+﻿SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+/*
+
+ ╭────────────────────────────────────────────────────────────────────────────────────────────────╮
+ │                                                                                                │
+ │          AUTHOR:  Aaron Priesterroth                                                           │
+ │         ────────                                                                               │
+ │                                                                                                │
+ │     CREATE DATE:  20/08/2022                                                                   │
+ │    ─────────────                                                                               │
+ │                                                                                                │
+ │     DESCRIPTION:  This procedure sets a specific server principal (login) as the owner of      │
+ │    ─────────────  every agent job.                                                             │
+ │                                                                                                │
+ │                   If no server principal is specified, the 'sa' login is used.                 │
+ │                                                                                                │
+ ├────────────────────────────────────────────────────────────────────────────────────────────────┤
+ │                                                                                                │
+ │                                                                                                │
+ │  CHANGE HISTORY:                                                                               │
+ │  ───────────────                                                                               │
+ │                                                                                                │
+ │                                                                                                │
+ │    DATE          AUTHOR                            COMMENT                                     │
+ │   ────────────  ───────────────────────────────── ──────────────────────────────────────────   │
+ │    2022-08-20    Aaron Priesterroth                Initial creation.                           │
+ │                                                                                                │
+ │                                                                                                │
+ ╰────────────────────────────────────────────────────────────────────────────────────────────────╯
+
+*/
+
+CREATE OR ALTER   PROCEDURE [dbo].[usp_set_owner_job]
+	@job_owner_new NVARCHAR(MAX) = 'sa'
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @sql      NVARCHAR(MAX);
+	DECLARE @job_name NVARCHAR(MAX);
+
+	IF (EXISTS(SELECT 1 FROM [sys].[server_principals] WHERE [name] = @job_owner_new))
+	BEGIN
+		--
+		SELECT [name] INTO [#jobs] FROM [msdb].[dbo].[sysjobs] WHERE [owner_sid] <> SUSER_SID(@job_owner_new);
+
+		WHILE(EXISTS(SELECT 1 FROM [#jobs]))
+		BEGIN
+			SET @job_name = (SELECT TOP(1) [name] FROM [#jobs] ORDER BY [name]);
+
+			SET @sql = 'USE [msdb]; EXEC [msdb].[dbo].[sp_update_job] @job_name=''' + @job_name + ''', @owner_login_name=''' + @job_owner_new + ''';';
+			EXEC sp_executesql @sql
+
+			DELETE FROM [#jobs] WHERE [name] = @job_name;
+		END;
+		
+		DROP TABLE [#jobs];
+	END;
+	ELSE
+	BEGIN
+		PRINT '
+ERROR:
+The login name ' + QUOTENAME(@job_owner_new) + ' could not be found.';
+	END;
+END
+GO
